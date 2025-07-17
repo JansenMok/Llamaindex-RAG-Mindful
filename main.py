6d0f64f6a152696428d25745346e1b656521f964
JansenMok <jansenmok@berkeley.edu>
2025-07-16 21:58:35 -0700

    add current source

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+# SimpleDirectoryReader: reads .txt article
+# GPTVectorStoreIndex: creates index to store/receive text chunks
+from llama_index.core import SimpleDirectoryReader, GPTVectorStoreIndex, Settings
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+
+# global settings
+# Settings.llm = Gemini(model="models/gemini-2.0-flash")
+Settings.llm = GoogleGenAI(model="gemini-2.0-flash")
+Settings.embed_model = GoogleGenAIEmbedding(model_name="text-embedding-004")
+
+articles = SimpleDirectoryReader("articles").load_data()  # load txt
+index = GPTVectorStoreIndex.from_documents(articles)  # txt -> chunks (500 tokens)
+
+query_engine = index.as_query_engine()  # RAG pipeline
+
+print(query_engine.query("Give me a summary of article1."))
