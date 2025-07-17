3554e47297c7faca4a163a9bccbe4fae412e132e
JansenMok <jansenmok@berkeley.edu>
2025-07-16 23:13:35 -0700

    add web frontend

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+import streamlit as st
 
 # SimpleDirectoryReader: reads .txt article
 # GPTVectorStoreIndex: creates index to store/receive text chunks
 from llama_index.core import SimpleDirectoryReader, GPTVectorStoreIndex, Settings
 from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 # global settings
 # Settings.llm = Gemini(model="models/gemini-2.0-flash")
@@ -19,8 +21,16 @@ query_engine = index.as_query_engine()  # RAG pipeline
 
 # print(query_engine.query("Give me a summary of article1."))
 
-while (True):
-    prompt = input("💬: ")
-    if (prompt == "end"):
-        exit()
-    print(query_engine.query(prompt))
+# cli frontend
+# while (True):
+#     prompt = input("💬: ")
+#     if (prompt == "end"):
+#         exit()
+#     print(query_engine.query(prompt))
+
+# streamlit frontend
+st.title("Amitabha Mindful QnA Chatbot")
+query = st.text_input("💬: ")
+if query:
+    answer = query_engine.query(query)
+    st.markdown(f"**Answer:** {answer}")
